f00ace3cde421f536432adcbf345321870321971
Stephane Bellity <sbellity@gmail.com>
2011-06-28 17:11:05 +0200

    Adding type detection on comments

--- lib/githu3/comment.rb
+++ lib/githu3/comment.rb
@@ -3,6 +3,18 @@ module Githu3
     
     embeds_one :user
     
+    def _type
+      m = url.match(/(issues|gists|pulls)\/comments\/[0-9]+$/)
+      if m.nil? || m[1].nil?
+        "commitcomment"
+      else
+        "#{m[1].singularize}comment"
+      end
+    end
+    
+    def _mime_type mt
+      "application/vnd.github-#{_type}.#{mt}+json"
+    end
     
   end
 end
